d54e3736cad7f3d626c6a2c964035b4677771ff1
4T5-SK8SP9 <jagki@pandora.net>
2026-06-30 14:25:14 +0200

    Change base path from '/team-health-check/' to '/productline-health-check/'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/team-health-check/',
+  base: '/productline-health-check/',
   root: '.',
   build: {
     outDir: 'dist'
